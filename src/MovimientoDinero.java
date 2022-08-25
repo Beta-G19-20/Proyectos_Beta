28d1cd784ae5b60ded2e36a948a0fde4cdac6512
felipeReinaA <felipereina1@hotmail.com>
2022-08-25 11:21:35 -0500

    clase MovimientoDinero

---
+++ src/MovimientoDinero.java
@@ -0,0 +1,29 @@
+public class MovimientoDinero {
+    private int monto;
+    private String concepto;
+    private int id;
+
+    public int getMonto() {
+        return monto;
+    }
+
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
