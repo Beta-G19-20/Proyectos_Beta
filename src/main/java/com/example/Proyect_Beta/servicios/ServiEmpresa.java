f6d724f5c6cfdcb03216208a856e3054f2d02c31
Gustavo Andres Becerra Sanchez <110946770+IngGustavoBecerra@users.noreply.github.com>
2022-09-07 20:13:08 -0500

    Actualizacion

--- src/main/java/com/example/Proyect_Beta/servicios/ServiEmpresa.java
+++ src/main/java/com/example/Proyect_Beta/servicios/ServiEmpresa.java
@@ -1,4 +1,22 @@
 package com.example.Proyect_Beta.servicios;
 
-public interface ServiEmpresa {
+import com.example.Proyect_Beta.entidades.Empresa;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ServiEmpresa
+{
+    public List<Empresa> listarEmpresas();
+    public Empresa guardarEmpresas(Empresa empresa);
+    public Empresa consultarEmpresasPorId(Integer nit);
+    public Empresa actualizarEmpresas(Empresa empresa);
+    public void eliminarEmpresas(Integer nit);
+
+    public Empresa actEmpresasId(Integer id, Map<Object,Object> objectMap);
+
+
+
+
+
 }
