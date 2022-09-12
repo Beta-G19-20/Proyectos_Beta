aa15feeb777c147e808a7a78786e0422559aa99d
Gustavo Andres Becerra Sanchez <110946770+IngGustavoBecerra@users.noreply.github.com>
2022-09-11 21:59:56 -0500

    Actualizacion

---
+++ src/main/java/com/example/Proyect_Beta/controlador/ControladorMovimientoDinero.java
@@ -0,0 +1,58 @@
+package com.example.Proyect_Beta.controlador;
+
+import com.example.Proyect_Beta.entidades.MovimientoDinero;
+import com.example.Proyect_Beta.servicios.ServicioImpMovimientoDinero;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+//@RequestMapping("/enterprises/{id}/movements")
+@RequestMapping("/movimientos")
+@RestController
+
+
+public class ControladorMovimientoDinero {
+
+    @Autowired
+    private ServicioImpMovimientoDinero sic;
+
+
+
+    @GetMapping
+    public List<MovimientoDinero> listar(){
+        return sic.listarMovimientoDinero();
+    }
+
+    @GetMapping("/{id}")
+    public MovimientoDinero consultarPorId(@PathVariable("id") Integer idMovimiento) {
+        return sic.consultarMovimientosPorId(idMovimiento);
+    }
+
+    @PostMapping
+    public MovimientoDinero insertar(@RequestBody MovimientoDinero idMovimiento){
+        return sic.guardarMovimientoDineroPorID(idMovimiento);
+    }
+
+    @PutMapping
+    public MovimientoDinero actualizar(@RequestBody MovimientoDinero idMovimiento){
+        return sic.actualizarMovimientoDinero(idMovimiento);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarporId(@PathVariable("id") Integer idMovimiento){
+        sic.eliminarMovimientoDineroId(idMovimiento);
+    }
+
+    @PatchMapping("/{id}")
+    public MovimientoDinero actualizarpor(@PathVariable("id")Integer id, @RequestBody Map<Object,Object> objectMap){
+        return sic.actualizarPorId(id,objectMap);
+    }
+
+    //@PatchMapping("/{id}/")
+    //public MovimientoDinero consultarMovPorId(@PathVariable("id")Integer id, @RequestBody Map<Object,Object> objectMap){
+    //    return sic.consultarMovPorId(id,objectMap);
+    //}
+
+}
---
+++ src/main/java/com/example/Proyect_Beta/repositorio/RepositorioMovimientoDinero.java
@@ -0,0 +1,8 @@
+package com.example.Proyect_Beta.repositorio;
+
+import com.example.Proyect_Beta.entidades.MovimientoDinero;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositorioMovimientoDinero extends JpaRepository<MovimientoDinero, Integer> {
+}
